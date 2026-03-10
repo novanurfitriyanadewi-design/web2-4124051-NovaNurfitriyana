638b526e2b900bb1ff09d93bd3231062bfad6b3b
nova <novanurfitriyanadewi@saintek.unipdu.ac.id>
2026-03-10 15:37:46 +0700

    Menambahkan halaman profil

---
+++ app/Http/Controllers/ProfilController.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ProfilController extends Controller
+{
+    public function index()
+    {
+        // Data organisasi
+        $organisasi = [
+            [
+                'nama' => 'BEM SAINTEK',
+                'deskripsi' => 'Badan Eksekutif Mahasiswa merupakan lembaga eksekutif yang menjalankan program kerja dan menyalurkan aspirasi mahasiswa.',
+                'visi' => 'Mewujudkan organisasi mahasiswa yang aktif, kreatif, dan solidaritas.',
+                'misi' => [
+                    'Menyelenggarakan kegiatan mahasiswa',
+                    'Menyalurkan aspirasi mahasiswa',
+                    'Meningkatkan solidaritas antar mahasiswa'
+                ]
+            ],
+            [
+                'nama' => 'DPM SAINTEK',
+                'deskripsi' => 'Dewan Perwakilan Mahasiswa adalah lembaga legislatif yang mengawasi kinerja BEM dan membuat peraturan organisasi mahasiswa.',
+                'visi' => 'Mewujudkan organisasi mahasiswa yang transparan dan demokratis.',
+                'misi' => [
+                    'Mengawasi program kerja BEM',
+                    'Menyusun peraturan organisasi',
+                    'Mewakili aspirasi mahasiswa'
+                ]
+            ],
+            [
+                'nama' => 'HIMA SISTEM INFORMASI',
+                'deskripsi' => 'Himpunan Mahasiswa merupakan organisasi mahasiswa tingkat program studi yang bertujuan mengembangkan potensi mahasiswa.',
+                'visi' => 'Menjadikan mahasiswa yang aktif dan inovatif.',
+                'misi' => [
+                    'Mengadakan kegiatan akademik',
+                    'Mengembangkan potensi mahasiswa',
+                    'Meningkatkan solidaritas mahasiswa'
+                ]
+            ]
+        ];
+
+        // Data pengurus
+        $pengurus = [
+            [
+                'nama' => 'Nova Nurfitriyana',
+                'nim' => '4124051',
+                'jabatan' => 'Anggota BEM SAINTEK',
+                'prodi' => 'Sistem Informasi',
+                'semester' => '4',
+                'keahlian' => ['Manajemen Organisasi','Public Speaking','Laravel']
+            ],
+            [
+                'nama' => 'Bimo Adi Nugroho',
+                'nim' => '4124013',
+                'jabatan' => 'Anggota BEM SAINTEK',
+                'prodi' => 'Sistem Informasi',
+                'semester' => '4',
+                'keahlian' => ['Administrasi','Microsoft Office','PHP']
+            ],
+            [
+                'nama' => 'Moh. Badrus Sholeh',
+                'nim' => '4124053',
+                'jabatan' => 'Anggota DPM SAINTEK',
+                'prodi' => 'Sistem Informasi',
+                'semester' => '4',
+                'keahlian' => ['Akuntansi','Manajemen Keuangan','MySQL']
+            ]
+        ];
+
+        return view('profil', compact('organisasi','pengurus'));
+    }
+
+    public function show($nim)
+    {
+        return "Profil pengurus dengan NIM: " . $nim;
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,24 +1,40 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProfilController;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
 Route::get('/perkenalan', function () {
-
     return '<h1>Halo! Nama saya Nova Nurfitriyana </h1>
-
             <p>NIM: 4124051 | Prodi: Sistem Informasi</p>
-
             <p>Saya siap belajar Laravel! 🚀</p>';
-
 });
+
 Route::get('/badrus-sholeh', function () {
     return "Halo, saya Badrus Sholeh (Kolaborator)";
-});            
+});
 
 Route::get('/bimoadi', function () {
-    return "Halo, saya bimoadi (Kolaborator)";
-}); 
+    return "Halo, saya Bimoadi (Kolaborator)";
+});
+
+/* ROUTE NOVA */
+
+Route::get('/tentang', function () {
+    return "<h1>Tentang Sistem Administrasi ORMAWA</h1>
+            <p>Website ini dibuat untuk mengelola administrasi organisasi mahasiswa SAINTEK UNIPDU Jombang.</p>";
+});
+
+Route::get('/contact', function () {
+    return "<h1>Contact ORMAWA</h1>
+            <p>Email: ormawa@saintek.ac.id</p>";
+});
+
+/* ROUTE PROFIL */
+
+Route::get('/profil', [ProfilController::class, 'index']);
+
+Route::get('/profil/{id}', [ProfilController::class, 'show']);
